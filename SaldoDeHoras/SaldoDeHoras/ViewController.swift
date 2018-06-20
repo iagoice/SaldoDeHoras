f712bff275e5e7952cc1b5a5bc75596eae90555b
Iago Mordente <iagoc@ciandt.com>
2018-06-20 15:38:21 -0300

    criação do projeto

---
+++ SaldoDeHoras/SaldoDeHoras/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaldoDeHoras
+//
+//  Created by Iago Mordente Rezende Leão Corrêa on 20/06/18.
+//  Copyright © 2018 Iago Mordente Rezende Leão Corrêa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
