6d7d378b889a55ed1054a0af7bb342b24295da2d
Iago Mordente <iagoc@ciandt.com>
2018-07-02 17:08:53 -0300

    Adicionada a funcionalidade de pagar as horas do saldo de horas

---
+++ SaldoDeHoras/SaldoDeHoras/Common/Extensions and Protocols/UIViewExtension.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewExtension.swift
+//  SaldoDeHoras
+//
+//  Created by Iago Mordente Rezende Leão Corrêa on 29/06/18.
+//  Copyright © 2018 Iago Mordente Rezende Leão Corrêa. All rights reserved.
+//
+
+import UIKit
+
+class UIViewExtension: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SaldoDeHoras/SaldoDeHoras/Common/Extensions and Protocols/UserExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UserExtension.swift
+//  SaldoDeHoras
+//
+//  Created by Iago Mordente Rezende Leão Corrêa on 02/07/18.
+//  Copyright © 2018 Iago Mordente Rezende Leão Corrêa. All rights reserved.
+//
+
+import Foundation
